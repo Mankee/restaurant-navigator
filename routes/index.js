11d51052cf1149c0d67324726db21525d8b7be26
Danielle H <daheber@gmail.com>
2017-06-24 10:08:11 -0700

    start user login and registration

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const storeController = require('../controllers/storeController');
+const userController = require('../controllers/userController');
 const { catchErrors } = require('../handlers/errorHandlers');
 
 router.get('/', catchErrors(storeController.getStores));
@@ -18,5 +19,10 @@ router.post('/add/:id',
 );
 router.get('/stores/:id/edit', catchErrors(storeController.editStore));
 router.get('/store/:slug', catchErrors(storeController.getStoreBySlug));
+
+// login and registration
+router.get('/register', userController.registerForm);
+router.get('/login', userController.loginForm);
+router.post('/register', userController.validateRegister);
 
 module.exports = router;
