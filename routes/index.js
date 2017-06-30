8746db94013ed84b2edf56f4a1bc280bb7a9ec98
Danielle H <daheber@gmail.com>
2017-06-30 15:59:34 -0700

    login/logout middleware and protect /add route

--- routes/index.js
+++ routes/index.js
@@ -7,7 +7,7 @@ const { catchErrors } = require('../handlers/errorHandlers');
 
 router.get('/', catchErrors(storeController.getStores));
 router.get('/stores', catchErrors(storeController.getStores));
-router.get('/add', storeController.addStore);
+router.get('/add', authController.isLoggedIn, storeController.addStore);
 router.post('/add', 
   storeController.upload, 
   catchErrors(storeController.resize), 
@@ -26,10 +26,13 @@ router.get('/tags/:tag', catchErrors(storeController.getStoresByTag));
 // login and registration
 router.get('/register', userController.registerForm);
 router.get('/login', userController.loginForm);
+router.post('/login', authController.login);
 router.post('/register', 
   userController.validateRegister,
   userController.register,
   authController.login
 );
+
+router.get('/logout', authController.logout);
 
 module.exports = router;
